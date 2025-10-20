469029b4c4027176dcf3e3831d5fdb9fb876c91f
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-10-20 22:11:32 +0900

    feat: 컨트롤러를 통한 애플리케이션 진입점 정의

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import calculator.controller.Controller;
+import calculator.factory.CalculatorFactory;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = CalculatorFactory.createController();
+        controller.run();
     }
 }
